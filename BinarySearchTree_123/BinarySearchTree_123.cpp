8660f8ee8c2ac0541776c89b6f219175d6f2bbe7
iamanggi <anggipuspita452@gmail.com>
2023-06-30 12:05:56 +0700

    melakukan step 6 pada void insert

--- BinarySearchTree_123/BinarySearchTree_123.cpp
+++ BinarySearchTree_123/BinarySearchTree_123.cpp
@@ -44,5 +44,10 @@ public:
 			ROOT = NewNode;  // mark the new node as ROOT
 			return; //Exit
 		}
+
+		if (element < parent->info) // if the value in the data field of the new node is less than that of the parent
+		{
+			parent->leftchild = NewNode; //make the left child of the parent point to the new node
+		}
 	}
 };
