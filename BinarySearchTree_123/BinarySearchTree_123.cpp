c891ae2d3d21b89c7049f7614b9a16a8946707a8
iamanggi <anggipuspita452@gmail.com>
2023-06-30 12:20:52 +0700

    membuat void postorder

--- BinarySearchTree_123/BinarySearchTree_123.cpp
+++ BinarySearchTree_123/BinarySearchTree_123.cpp
@@ -94,4 +94,17 @@ public:
 			preorder(ptr->rightchild);
 		}
 	}
+
+	void postorder(node* ptr) {
+		//performs the postorder traversal of the tree
+		if (ROOT == NULL) {
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << "  ";
+		}
+	}
 };
